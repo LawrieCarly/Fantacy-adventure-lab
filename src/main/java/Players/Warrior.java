4bd4cac3140605256f40671805364e1e801d1b57
Ethan <ethanbaird94@gmail.com>
2022-09-14 14:35:46 +0100

    adds fatal blow to Warrior.attack()

--- src/main/java/Players/Warrior.java
+++ src/main/java/Players/Warrior.java
@@ -31,23 +31,51 @@ public class Warrior extends Player implements IAttack {
     public void attack(Player player) {
         int attackPoints = this.weapon.getAttackPoints();
 
+        boolean fatal = false;
+
+        // handles attack against Warrior
         if ((player) instanceof Warrior){
             WarriorType warriorType = ((Warrior) player).getWarriorType();
             int resistance = warriorType.getWeaponResistance();
-            player.healthPoints -= (attackPoints - resistance);
+            int currentHealth = player.getHealthPoints();
+            if (attackPoints >= currentHealth + resistance){
+                fatal = true;
+            }
+            if (!fatal){
+                player.healthPoints -= (attackPoints - resistance);
+            }
         }
+
+        // handles attack against Caster
         if ((player) instanceof Caster){
             CasterType casterType = ((Caster) player).getCasterType();
             int resistance = casterType.getWeaponResistance();
             int defencePoints = ((Caster) player).defend();
-            if(defencePoints + resistance <= attackPoints){
+            int currentHealth = player.getHealthPoints();
+            if (attackPoints >= currentHealth + resistance + defencePoints){
+                fatal = true;
+            }
+            if(!fatal && defencePoints + resistance <= attackPoints){
                 player.healthPoints -= (attackPoints - resistance - defencePoints);
             }
         }
+
+        //handles attack against Healer
         if ((player) instanceof Healer){
             HealerType healerType = ((Healer) player).getHealerType();
             int resistance = healerType.getWeaponResistance();
-            player.healthPoints -= (attackPoints - resistance);
+            int currentHealth = player.getHealthPoints();
+            if (attackPoints >= currentHealth + resistance){
+                fatal = true;
+            }
+            if (!fatal){
+                player.healthPoints -= (attackPoints - resistance);
+            }
+        }
+
+        // handles fatal blow
+        if (fatal){
+            player.die();
         }
     }
 }
